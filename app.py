465a6bb243a66ca75cdb5b78b2e224fa4a9346ac
Daniel42560 <109819554+Daniel42560@users.noreply.github.com>
2024-10-22 18:18:52 +0200

    Integrated Audio + Multiple Outputs

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+from pydub import AudioSegment
 import os
 import gradio as gr
 import requests
@@ -8,12 +9,16 @@ from PIL import Image
 
 load_dotenv()
 
-API_URL = "https://api-inference.huggingface.co/models/sd-community/sdxl-flash"
+API_URL_IMAGE = "https://api-inference.huggingface.co/models/sd-community/sdxl-flash"
+API_URL_AUDIO = "https://api-inference.huggingface.co/models/facebook/musicgen-small"
+
 headers = {"Authorization": "Bearer " + os.getenv("HUGGINGFACE_TOKEN")}
 
 
-def query(payload):
-    response = requests.post(API_URL, headers = headers, json = payload)
+
+
+def query(payload, api_url):
+    response = requests.post(api_url, headers = headers, json = payload)
 
     # Check if the status code indicates success
     if response.status_code != 200:
@@ -22,10 +27,16 @@ def query(payload):
     return response.content
 
 def generate_image(prompt):
-    image_bytes = query({"inputs": prompt})
+    image_bytes = query({"inputs": prompt}, API_URL_IMAGE)
 
     image = Image.open(io.BytesIO(image_bytes))
     return image
+
+def generate_audio(prompt):
+    audio_bytes = query({"inputs": prompt}, API_URL_AUDIO)
+    print("Hier steht der typ:", type(audio_bytes))
+    #audio = AudioSegment.from_file(io.BytesIO(audio_bytes), format="mp3")  # e.g., "mp3", "wav", etc.
+    return audio_bytes
 
 #gradio interface
 
@@ -44,5 +55,11 @@ with gr.Blocks() as blocks:
     output = gr.Image(label = "Output Image")
     submit_btn = gr.Button("Submit")
     submit_btn.click(fn = generate_image, inputs=prompt, outputs=output)
+
+    audio_prompt = gr.Textbox(label ="Audio Prompt")
+    audio_output = gr.Audio(label="Output Audio")
+    audio_submit_btn = gr.Button("Submit Audio Prompt")
+    audio_submit_btn.click(fn = generate_audio, inputs=audio_prompt, outputs=audio_output)
+    
 
 blocks.launch(share = True)
