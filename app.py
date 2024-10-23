139d7de1179d5b138234c3c9b45395e1eccd0422
c-aranda-bassegoda <carolina.aranda.2699@gmail.com>
2024-10-23 10:47:32 +0200

    text to speech done

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ load_dotenv()
 
 API_URL_IMAGE = "https://api-inference.huggingface.co/models/stable-diffusion-v1-5/stable-diffusion-v1-5"
 API_URL_AUDIO = "https://api-inference.huggingface.co/models/facebook/musicgen-small"
+API_URL_TOSPEECH = "https://api-inference.huggingface.co/models/facebook/mms-tts-eng"
 
 headers = {"Authorization": "Bearer " + os.getenv("HUGGINGFACE_TOKEN")}
 
@@ -37,6 +38,9 @@ def generate_audio(prompt):
     #audio = AudioSegment.from_file(io.BytesIO(audio_bytes), format="mp3")  # e.g., "mp3", "wav", etc.
     return audio_bytes
 
+def generate_speech(prompt):
+    speech_bytes = query({"inputs": prompt}, API_URL_TOSPEECH)
+    return speech_bytes
 
 def chatbot_response(message):
     # Make the request to the model
@@ -59,6 +63,10 @@ with gr.Blocks() as blocks:
     chat_output = gr.Textbox(label="Response", interactive=False)
     chat_submit_btn = gr.Button("Send")
     chat_submit_btn.click(fn=chatbot_response, inputs=chat_input, outputs=chat_output)
+    
+    speech_output = gr.Audio(label="Output Audio")
+    speech_submit_btn = gr.Button("Narrate")
+    speech_submit_btn.click(fn = generate_speech, inputs=chat_output, outputs=speech_output)
 
     with gr.Row():
         with gr.Column(scale=3):
@@ -75,6 +83,7 @@ with gr.Blocks() as blocks:
     audio_output = gr.Audio(label="Output Audio")
     audio_submit_btn = gr.Button("Submit Audio Prompt")
     audio_submit_btn.click(fn = generate_audio, inputs=audio_prompt, outputs=audio_output)
+    
 
 if __name__ == "__main__":
     blocks.launch(share=False)
